e3e5ab29bd644d1f9be8b95d281aadd3eea50384
Takuya Tokuda <cs.toku.mail@gmail.com>
2025-12-14 12:22:35 +0900

    fix: separate envs

--- app/config.py
+++ app/config.py
@@ -14,12 +14,13 @@ PRIVILEGE_EXPIRATION_IN_SECONDS = 3600
 
 # OPENAI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-# OPENAI_API_BASE_URL = "https://api.openai.com/v1"
-# OPENAI_MODEL_ID= "gpt-4o-mini"
-# OPENAI_EMBEDDING_MODEL_ID = "text-embedding-3-small"
-OPENAI_API_BASE_URL = "https://api.ai.sakura.ad.jp/v1"
+OPENAI_API_BASE_URL = "https://api.openai.com/v1"
 OPENAI_MODEL_ID= "Qwen3-Coder-480B-A35B-Instruct-FP8"
 OPENAI_EMBEDDING_MODEL_ID = "multilingual-e5-large"
+# OPENAI_MODEL_ID= "gpt-4o-mini"
+# OPENAI_EMBEDDING_MODEL_ID = "text-embedding-3-small"
+SAKURANOAI_API_KEY = os.getenv("SAKURANOAI_API_KEY")
+SAKURANOAI_API_BASE_URL = "https://api.ai.sakura.ad.jp/v1"
 
 # システム設定
 LOG_LEVEL = "INFO"
